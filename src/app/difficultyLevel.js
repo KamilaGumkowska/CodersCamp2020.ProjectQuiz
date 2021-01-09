cd43e539dffd4414e6bc4931d3e917258811a20f
Mateusz Łepecki <lepek__13@wp.pl>
2021-01-09 14:06:33 +0100

    A some CSS to difficulty screen

--- src/app/difficultyLevel.js
+++ src/app/difficultyLevel.js
@@ -1,6 +1,6 @@
 export { showDifficultyLevelScreen };
 
-const LEVEL_NAMES = ['Easy', 'Medium', 'Hard'];
+const LEVEL_NAMES = ['EASY', 'MEDIUM', 'HARD'];
 const MAIN = document.querySelector('#quiz-app');
 
 
@@ -14,13 +14,14 @@ function showDifficultyLevelScreen(quizSettings) {
 
 const createHTMLStructure = () => {
     MAIN.innerHTML = '';
-    let numberOfQuestionsContainer = createDOMElement('div', 'numberOfQuestionsContainer', MAIN);
-    let levelContainer = createDOMElement('div', 'levelContainer', MAIN);
+    let levelScreenWrapper = createDOMElement('div', 'levelScreenWrapper', MAIN);
+    let numberOfQuestionsContainer = createDOMElement('div', 'numberOfQuestionsContainer', levelScreenWrapper);
+    let levelContainer = createDOMElement('div', 'levelContainer', levelScreenWrapper);
     let numberOfQuestionsParagraph = createDOMElement(
-        'div',
-        'numberOfQuestionsParagraph',
+        'p',
+        'numberOfQuestionsParagraph font',
         numberOfQuestionsContainer,
-        'Number of questions',
+        'NUMBER OF QUESTIONS'
     );
     levelContainer.innerHTML = `<input type="range" class="differenceLevel" min="0" max="2" step="1"/>`;
 
@@ -34,13 +35,14 @@ const createHTMLStructure = () => {
     <input type="range" id="numberOfQuestionsInput" class="range numberOfQuestions" min="10" max="50" step="1"/>`;
     let levelLabelsContainer = createDOMElement('div', 'levelLabelsContainer', levelContainer);
     LEVEL_NAMES.forEach((el, index) => {
-        createDOMElement('p', 'levelLabel', levelLabelsContainer, LEVEL_NAMES[index]);
+        createDOMElement('p', 'levelLabel font', levelLabelsContainer, LEVEL_NAMES[index]);
     });
-    let backToHomePageButton = createDOMElement('button', 'backToHomePageButton', MAIN);
-    let confirmLevelButton = createDOMElement('button', 'confirmLevelButton', MAIN);
-    backToHomePageButton.textContent = "Back to home";
-    backToHomePageButton.textContent
-    confirmLevelButton.textContent = "Let's play!";
+    let buttonsContainer = createDOMElement('div', 'buttonsContainer', levelScreenWrapper);
+    let backToCategoryPageButton = createDOMElement('button', 'backToCategoryPageButton font', buttonsContainer);
+    let confirmLevelButton = createDOMElement('button', 'confirmLevelButton font', buttonsContainer);
+    backToCategoryPageButton.textContent = "BACK";
+    backToCategoryPageButton.setAttribute('onclick', "window.location.href='#quiz-app'");
+    confirmLevelButton.textContent = "LET'S PLAY!";
 
 };
 const createDOMElement = function (type, className, parent, text = '') {
@@ -66,7 +68,7 @@ function setBubble(range, bubble) {
     const max = range.max ? range.max : 100;
     const newVal = Number(((val - min) * 100) / (max - min));
     bubble.innerHTML = val;
-    bubble.style.left = `calc(${newVal}% + (${20 - newVal * 0.35}px))`;
+    bubble.style.left = `calc(${newVal}% + (${13 - newVal * 0.20}px))`;
 }
 
 const updateSettings = (quizSettings) => {
@@ -76,6 +78,5 @@ const updateSettings = (quizSettings) => {
         let selectedNbrOfQuestions = document.querySelector('.numberOfQuestions').value;
         quizSettings.difficultyLevel = selectedLevel;
         quizSettings.numberOfQuestion = selectedNbrOfQuestions;
-        console.log(quizSettings);
     });
 };
