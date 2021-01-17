0befcbc98a3d677e5d71b702f47e0a61e62aef58
Mateusz Łepecki <lepek__13@wp.pl>
2021-01-17 11:12:03 +0100

    C add some const variable

--- src/app/optionsScreen.js
+++ src/app/optionsScreen.js
@@ -3,6 +3,15 @@ import { createImageSection } from './difficultyLevel';
 import { createDOMElement } from './difficultyLevel';
 import { music } from './music';
 
+const PARAGRAPH_ELEMENT = 'p';
+const DIV_ELEMENT = 'div';
+const INPUT_ELEMENT = 'input';
+const BUTTON_ELEMENT = 'button';
+const SET_DEFAULT_NICKNAME = 'SET DEFAULT NICKNAME';
+const RESET_LEADERBOARD = 'RESET LEADERBOARD';
+const FIRST_MUSIC_TEXT = 'MUSIC ON';
+const SUBMIT_TEXT = 'SUBMIT';
+
 const MAIN = document.querySelector('#main-wrap');
 export let defaultNickFromOptions;
 export function showOptionsScreen() {
@@ -11,25 +20,25 @@ export function showOptionsScreen() {
 
 const createHTMLStructure = () => {
     MAIN.innerHTML = '';
-    let optionsScreen = createDOMElement('div', 'optionsScreen', MAIN);
+    let optionsScreen = createDOMElement(DIV_ELEMENT, 'optionsScreen', MAIN);
     createOptions(optionsScreen);
     createBackButton(optionsScreen);
     createImageSection(optionsScreen);
 };
 const createOptions = (parent) => {
-    let OptionsButtonsContainer = createDOMElement('div', 'OptionsButtonsContainer', parent);
+    let OptionsButtonsContainer = createDOMElement(DIV_ELEMENT, 'OptionsButtonsContainer', parent);
     let resetLeaderboard = createDOMElement(
-        'button',
+        BUTTON_ELEMENT,
         'resetLeaderboard btn secondary optionButton OptionFont',
         OptionsButtonsContainer,
     );
     let musicButton = createDOMElement(
-        'button',
+        BUTTON_ELEMENT,
         'musicButton btn secondary optionButton OptionFont',
         OptionsButtonsContainer,
     );
-    resetLeaderboard.textContent = 'RESET LEADERBOARD';
-    musicButton.textContent = 'MUSIC ON';
+    resetLeaderboard.textContent = RESET_LEADERBOARD;
+    musicButton.textContent = FIRST_MUSIC_TEXT;
     resetLeaderboard.addEventListener('click', () => {
         console.log('RESET LEADERBOARD');
         localStorage.setItem = 'test local storage';
@@ -39,12 +48,12 @@ const createOptions = (parent) => {
 };
 
 const createNickInput = (parent) => {
-    let nickContainer = createDOMElement('div', 'nickContainer', parent);
+    let nickContainer = createDOMElement(DIV_ELEMENT, 'nickContainer', parent);
     createNickParagraph(nickContainer);
-    let nickInput = createDOMElement('input', 'nickInput btn secondary OptionFont', nickContainer);
-    nickInput.setAttribute('placeholder','YOUR NICKNAME');
-    let nickSubmit = createDOMElement('button', 'nickSubmit btn teritary optionButton', nickContainer);
-    nickSubmit.innerHTML = 'SUBMIT';
+    let nickInput = createDOMElement(INPUT_ELEMENT, 'nickInput btn secondary OptionFont', nickContainer);
+    nickInput.setAttribute('placeholder', 'YOUR NICKNAME');
+    let nickSubmit = createDOMElement(BUTTON_ELEMENT, 'nickSubmit btn teritary optionButton', nickContainer);
+    nickSubmit.innerHTML = SUBMIT_TEXT;
     nickSubmit.addEventListener('click', () => {
         let nickInput = document.querySelector('.nickInput');
         defaultNickFromOptions = nickInput.value;
@@ -54,11 +63,16 @@ const createNickInput = (parent) => {
 };
 
 const createNickParagraph = (parent) => {
-    let nickParagraphOption = createDOMElement('p', 'nickParagraphOption font', parent, 'SET DEFAULT NICKNAME');
+    let nickParagraphOption = createDOMElement(
+        PARAGRAPH_ELEMENT,
+        'nickParagraphOption font',
+        parent,
+        SET_DEFAULT_NICKNAME,
+    );
 };
 
 const createBackButton = (parent) => {
-    let backButtonContainer = createDOMElement('div', 'backButtonContainer', parent);
+    let backButtonContainer = createDOMElement(DIV_ELEMENT, 'backButtonContainer', parent);
     let backToMainPageButton = createDOMElement(
         'button',
         'backToMainPageButton btn primary OptionFont',
