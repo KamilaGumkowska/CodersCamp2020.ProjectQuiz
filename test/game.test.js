b6b0a41b15b23d27ba813eea2c38ad92a2000d3a
Kasia <katarzyna.ratajska@outlook.com>
2021-01-17 20:49:54 +0100

    Css files moved, tests WIP

---
+++ test/game.test.js
@@ -0,0 +1,7 @@
+// const shuffleAnswers = require('../src/app/game');
+
+// test('Properly sets icon to the chosen category', () => {
+//     expect(
+//         shuffleAnswers([1, 2, 3]).toEqual([1, 2, 3] || [2, 1, 3] || [3, 2, 1] || [1, 3, 2] || [3, 1, 2] || [2, 3, 1]),
+//     );
+// });
