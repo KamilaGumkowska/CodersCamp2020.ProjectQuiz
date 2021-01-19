d94f52199b098bc33291ef12e6ec7101a27a7692
Szymon <szymonpakula98@gmail.com>
2021-01-19 07:11:06 +0100

    removed failing test suite

--- test/game.test.js
+++
@@ -1,7 +0,0 @@
-// const shuffleAnswers = require('../src/app/game');
-
-// test('Properly sets icon to the chosen category', () => {
-//     expect(
-//         shuffleAnswers([1, 2, 3]).toEqual([1, 2, 3] || [2, 1, 3] || [3, 2, 1] || [1, 3, 2] || [3, 1, 2] || [2, 3, 1]),
-//     );
-// });
